d78989d6248a8d61acf85a8425008044fe2d355a
vfabz01 <vlad.romanow@mail.ru>
2020-09-16 15:03:22 +0300

    Create main.cpp
    starting

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+ cout<<"Hello DROG"<<endl;	
+ 
+	system("pause");
+	return 0;
+}
