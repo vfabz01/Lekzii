79021aa2a06099468005eff243f69fb872e95b2c
vfabz01 <vlad.romanow@mail.ru>
2020-09-18 13:04:44 +0300

    2
    2222222

--- main.cpp
+++ main.cpp
@@ -4,7 +4,34 @@
 using namespace std;
 
 int main(int argc, char** argv) {
- cout<<"Hello mazafaka"<<endl;	
+int mass [4] = {0,1,2,3};
+int cmd;
+while(1){
+}
+cout<<"0-exit,1-print,2-idx,3-sort"<<endl;
+cout<<"vvedite chislo"<<endl;
+cin>>cmd;
+
+switch (cmd)
+	{	
+	case 0:{
+		return 0;
+		break;
+}
+	case 1:{
+		for(int i;i<4;i++)
+		cout<<mass[4];
+		cout<<endl;      
+		break;
+	}
+	case 2:{
+		
+		break;
+	}
+	case 3:{
+		break;
+	}	
+	}
  
 	system("pause");
 	return 0;
